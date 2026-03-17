51c85dbb3d35f098c76fc94d90444a22a9abc313
Sergey Kazenyuk <git.v63hf@sergey-kazenyuk.com>
2026-03-17 20:49:02 +0000

    flake: move non-default kernel example to rpi4 system

--- flake.nix
+++ flake.nix
@@ -251,6 +251,29 @@
           {
             boot.tmp.useTmpfs = true;
           }
+
+          # Advanced: Use non-default kernel from kernel-firmware bundle
+          ({ config, pkgs, lib, ... }: let
+            kernelBundle = pkgs.linuxAndFirmware.v6_6_31;
+          in {
+            boot = {
+              loader.raspberry-pi.firmwarePackage = kernelBundle.raspberrypifw;
+              kernelPackages = kernelBundle.linuxPackages_rpi4;
+            };
+
+            nixpkgs.overlays = lib.mkAfter [
+              (self: super: {
+                # This is used in (modulesPath + "/hardware/all-firmware.nix") when at least 
+                # enableRedistributableFirmware is enabled
+                # I know no easier way to override this package
+                inherit (kernelBundle) raspberrypiWirelessFirmware;
+                # Some derivations want to use it as an input,
+                # e.g. raspberrypi-dtbs, omxplayer, sd-image-* modules
+                inherit (kernelBundle) raspberrypifw;
+              })
+            ];
+          })
+
         ];
       };
 
@@ -277,30 +300,6 @@
           {
             boot.tmp.useTmpfs = true;
           }
-
-          # Advanced: Use non-default kernel from kernel-firmware bundle
-          ({ config, pkgs, lib, ... }: let
-            kernelBundle = pkgs.linuxAndFirmware.v6_6_31;
-          in {
-            boot = {
-              loader.raspberry-pi.firmwarePackage = kernelBundle.raspberrypifw;
-              loader.raspberry-pi.bootloader = "kernel";
-              kernelPackages = kernelBundle.linuxPackages_rpi5;
-            };
-
-            nixpkgs.overlays = lib.mkAfter [
-              (self: super: {
-                # This is used in (modulesPath + "/hardware/all-firmware.nix") when at least 
-                # enableRedistributableFirmware is enabled
-                # I know no easier way to override this package
-                inherit (kernelBundle) raspberrypiWirelessFirmware;
-                # Some derivations want to use it as an input,
-                # e.g. raspberrypi-dtbs, omxplayer, sd-image-* modules
-                inherit (kernelBundle) raspberrypifw;
-              })
-            ];
-          })
-
         ];
       };
 
