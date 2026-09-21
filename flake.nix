3fd54b2b4addeaea25a83c1f1e1a527111d82be2
Sergey Kazenyuk <git.v63hf@sergey-kazenyuk.com>
2026-09-21 13:49:59 +0000

    flake: follow nixos-raspberrypi's develop

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-raspberrypi = {
-      url = "github:nvmd/nixos-raspberrypi/main";
+      url = "github:nvmd/nixos-raspberrypi/develop";
     };
 
     disko = {
